575d58d83a986da3903cdee9350c906e46ef652d
issy <issy@issy.dev>
2026-06-17 18:29:28 +0100

    Add args

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
+use std::path::PathBuf;
+use serde::Deserialize;
+
+#[derive(Deserialize)]
+#[serde(expecting = "")]
+#[serde(rename_all = "kebab-case")]
+struct Args {
+    config_file: PathBuf,
+    #[serde(alias = "d")]
+    directory: Option<PathBuf>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let args: Args = match serde_args::from_env() {
+        Ok(args) => args,
+        Err(error) => {
+            println!("{error}");
+            return;
+        }
+    };
+    // TODO: Parse config file
+    // TODO: Find directory
+
 }
