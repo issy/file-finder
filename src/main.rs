8fac2b23e8ab53ec21698a5e2bb6eba7de4b6fdd
issy <issy@issy.dev>
2026-06-17 22:41:55 +0100

    Implement config reading

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use std::path::PathBuf;
 use serde::Deserialize;
+use std::env::current_dir;
+use std::fs::File;
+use std::path::PathBuf;
 
 include!(concat!(env!("OUT_DIR"), "/codegen.rs"));
 
@@ -12,6 +14,16 @@ struct Args {
     directory: Option<PathBuf>,
 }
 
+fn validate_directory(path: PathBuf) -> Result<PathBuf, String> {
+    if !path.exists() {
+        Err(format!("The path '{}' does not exist.", path.display()))
+    } else if !path.is_dir() {
+        Err(format!("The path '{}' is not a directory.", path.display()))
+    } else {
+        Ok(path)
+    }
+}
+
 fn main() {
     let args: Args = match serde_args::from_env() {
         Ok(args) => args,
@@ -20,7 +32,9 @@ fn main() {
             return;
         }
     };
-    // TODO: Parse config file
-    // TODO: Find directory
 
+    let file = File::open(args.config_file).unwrap();
+    let config: RulesConfig = serde_yaml_ng::from_reader(file).unwrap();
+
+    let directory = args.directory.map(validate_directory).map(Result::unwrap).unwrap_or(current_dir().unwrap());
 }
