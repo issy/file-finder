7bbc2a78646b20831aa055b34458f3308171c939
Issy Szemeti <48881813+issy@users.noreply.github.com>
2026-06-26 21:28:03 +0100

    Add commit hook (#11)
    * Add npx configuration
    
    * Move lint-staged config to ts
    
    * Add prettier
    
    * Add prettier job
    
    * Install
    
    * Naming

---
+++ lint-staged.config.ts
@@ -0,0 +1,10 @@
+import type { Configuration } from "lint-staged";
+
+const cargoBuild = "cargo build";
+
+export default {
+  "schema.yaml": () => cargoBuild,
+  "Cargo.(lock,toml)": () => cargoBuild,
+  "*.rs": (files) => [cargoBuild, `cargo fmt -- ${files.join(" ")}`],
+  "*.{yaml,yml,json,md,ts}": "npx prettier --write",
+} satisfies Configuration;
